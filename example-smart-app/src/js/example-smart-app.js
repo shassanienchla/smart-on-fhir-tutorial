55519f99959f026b6c6ae85d95bafd67a242c9bd
Shehab Hassanien <37417047+shassanienchla@users.noreply.github.com>
2025-08-27 11:25:38 -0700

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -15,9 +15,13 @@
                     type: 'Observation',
                     query: {
                       code: {
-                        $or: ['http://loinc.org|8302-2', 'http://loinc.org|8462-4',
-                              'http://loinc.org|8480-6', 'http://loinc.org|2085-9',
-                              'http://loinc.org|2089-1', 'http://loinc.org|55284-4']
+                        $or: ['http://loinc.org|8302-2', //Body Height
+                              'http://loinc.org|8462-4', //Diastolic bp
+                              'http://loinc.org|8480-6', //systolic bp
+                              'http://loinc.org|2085-9', //hdl
+                              'http://loinc.org|2089-1', //ldl
+                              'http://loinc.org|2089-5', //temp
+                              'http://loinc.org|55284-4'] //bp
                       }
                     }
                   });
